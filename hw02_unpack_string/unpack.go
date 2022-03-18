0f7f39d599be23acaaef77b80b9b23f16c6c770d
o.popov <o.popov@x5.ru>
2022-03-18 17:03:18 +0300

    hw02_unpack_string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -16,35 +16,27 @@ func Unpack(str string) (string, error) {
 
 	for i := 0; i < length; i++ {
 		char := chars[i]
-
-		if i == 0 && unicode.IsDigit(char) {
-			return "", ErrInvalidString
-		}
-
 		nextChar := rune(-1)
+
 		if length > (i + 1) {
 			nextChar = chars[i+1]
 		}
 
-		if unicode.IsDigit(nextChar) && unicode.IsDigit(char) {
+		if (i == 0 && unicode.IsDigit(char)) || (unicode.IsDigit(nextChar) && unicode.IsDigit(char)) {
 			return "", ErrInvalidString
 		}
 
 		if !unicode.IsDigit(nextChar) && !unicode.IsDigit(char) {
 			newStr = append(newStr, char)
-
 			continue
 		}
 
 		charInt, _ := strconv.Atoi(string(nextChar))
-
 		if unicode.IsDigit(char) || charInt == 0 {
 			continue
 		}
 
-		charRepeat := strings.Repeat(string(char), charInt)
-		charRune := []rune(charRepeat)
-
+		charRune := []rune(strings.Repeat(string(char), charInt))
 		newStr = append(newStr, charRune...)
 	}
 
